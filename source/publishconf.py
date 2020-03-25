0acb521d25eea6fb98dcd870008930ebc4c4ceff
kostyrko <mkostyrko@gmail.com>
2020-03-25 13:14:13 +0100

    All functional

--- source/publishconf.py
+++ source/publishconf.py
@@ -10,8 +10,9 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
+
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://kostyrko.github.io/zfrontu-blog/'
+SITEURL = ''
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
