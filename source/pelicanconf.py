6bcdc7a9f6de12705fde6654426adc52c482a60e
kostyrko <mkostyrko@gmail.com>
2020-05-15 16:16:43 +0200

    minor styles update

--- source/pelicanconf.py
+++ source/pelicanconf.py
@@ -112,7 +112,7 @@ COMBINED_CSS = True
 
 # setting Pygments code highlighter
 # from source/theme/static/css/pygments
-PYGMENTS_STYLE = 'sas'
+PYGMENTS_STYLE = 'tango'
 
 # few ideas taken from https://github.com/pzelnip/www.codependentcodr.com/blob/master/pelicanconf.py
 # set copyright year
